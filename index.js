cc5c946293407045d60cf329ff9d37fd54ebeaca
shir15a <shir95admon@gmail.com>
2021-04-18 16:01:03 +0300

    .

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var cors = require('cors')
+const cors = require('cors')
 
 const bankRoute = require("./routes/Bank.routes");
 
 const app = express();
 const port = 5000;
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors())
 
 
 app.use("/api/bankApi", bankRoute);
