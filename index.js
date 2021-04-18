9736badf9856c60664e470780c59d01df03d98c4
shir15a <shir95admon@gmail.com>
2021-04-18 14:02:06 +0300

    .

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var cors = require('cors')
 
 const bankRoute = require("./routes/Bank.routes");
 
@@ -8,9 +9,10 @@ const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors())
 app.use("/api/bankApi", bankRoute);
 
-app.listen(port, () => {
+
+app.listen(process.env.PORT || port, () => {
   console.log(`appleication start at ${port}`);
 });
